4cbfd29f036530a44405386713f9c265cf65a5c2
exquet <dimatopgg@mail.ru>
2025-04-12 15:32:33 +0500

    drawing and actions with circles (polygons)

--- Source.cpp
+++ Source.cpp
@@ -12,11 +12,25 @@ int main()
 	text.setCharacterSize(24);
 	text.setFillColor(sf::Color::White);
 	text.setStyle(sf::Text::Bold | sf::Text::Style::Italic);
+
 	sf::Text text2(font);
 	text2.setString(L"тест ало привет");
 	text2.setCharacterSize(16);
     text2.setFillColor(sf::Color::Yellow);
 	text2.setPosition(sf::Vector2f(0, 50));
+
+    sf::CircleShape circle(25.f);
+    circle.setFillColor(sf::Color::White);
+    circle.setPosition(sf::Vector2f(10, 100));
+
+    sf::CircleShape triangle(25.f, 3);
+    triangle.setFillColor(sf::Color::Cyan);
+    triangle.setPosition(sf::Vector2f(100, 100));
+
+    sf::CircleShape square(25.f, 4);
+    square.setFillColor(sf::Color::Magenta);
+    square.setPosition(sf::Vector2f(190, 100));
+
 
     while (window.isOpen())
     {
@@ -29,6 +43,9 @@ int main()
         window.clear();
         window.draw(text);
         window.draw(text2);
+		window.draw(circle);
+		window.draw(triangle);
+		window.draw(square);
         window.display();
     }
 }
